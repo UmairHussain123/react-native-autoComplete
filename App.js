1b2f0b31aaa37777648e576eb489b4687c2e00d1
UmairHussain123 <33773362+UmairHussain123@users.noreply.github.com>
2022-09-30 10:15:21 +0500

    update

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import AutoComplete from "react-native-expo-autocomplete-dropdown";
 
 export default function App() {
+  const handleCallback = (childData) => {
+    console.log("child", childData);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AutoComplete
+        data={[
+          { id: "1", title: "Apple" },
+          { id: "2", title: "banana" },
+          { id: "3", title: "Amrod" },
+          { id: "4", title: "pear" },
+          { id: "5", title: "pine Apple" },
+          { id: "6", title: "orange" },
+        ]}
+        widthBySide={30}
+        placeholder={"select fruit"}
+        placeholderTextColor={"green"}
+        borderBottomColor={"green"}
+        borderRadius={7}
+        borderBottomWidth={2}
+        borderRadiusList={10}
+        listElevation={7}
+        listBackgroundColor={"white"}
+        listTextColor={"black"}
+        parentCallback={handleCallback}
+      ></AutoComplete>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
   },
 });
